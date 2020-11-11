78d4ca480c6637fd21c85a2aa08d9349986faa37
Tom <1599tm@gmail.com>
2020-11-11 15:27:55 +0000

    Add in terrain, make many menial changes

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ using namespace std;
 
 #include "shaders\Shader.h"
 
-glm::mat4 objectRotation;
+glm::mat4 objectTransformation;
 
 Shader* myShader;  ///shader object 
 Shader* myBasicShader;
@@ -30,7 +30,8 @@ Shader* myBasicShader;
 float amount = 0;
 float temp = 0.002f;
 
-ThreeDModel model; //A threeDModel object is needed for each model loaded
+ThreeDModel plane; //A threeDModel object for the plane
+ThreeDModel terrain; //A threeDModel object for the terrain
 OBJLoader objLoader;	//this object is used to load the 3d models.
 ///END MODEL LOADING
 
@@ -53,15 +54,16 @@ float LightPos[4] = {0.0f, 0.0f, 1.0f, 0.0f};
 //
 int	mouse_x=0, mouse_y=0;
 bool LeftPressed = false;
-int screenWidth=600, screenHeight=600;
+int screenWidth=1280, screenHeight=720;
 
 //booleans to handle when the arrow keys are pressed or released.
 bool Left = false;
 bool Right = false;
 bool Up = false;
 bool Down = false;
-bool Home = false;
-bool End = false;
+bool A = false;
+bool D = false;
+bool SPACE = false;
 
 float spin=180;
 float speed=0;
@@ -92,14 +94,15 @@ void display()
 
 	glm::mat4 viewingMatrix = glm::mat4(1.0f);
 	
-	//translation and rotation for view
-	viewingMatrix = glm::translate(glm::mat4(1.0), glm::vec3(0, 0, -50));
+	//environment view
+	viewingMatrix = glm::lookAt(glm::vec3(10, 15, -10), pos, glm::vec3(0.0f, 1.0f, 0.0));
+
 	/*static float angle = 0.0f;
 	angle += 0.0001;
 	viewingMatrix = glm::rotate(viewingMatrix, angle, glm::vec3(1.0f, 0.0f, 0.0));*/
 
-	//use of glm::lookAt
-	//viewingMatrix = glm::lookAt(glm::vec3(-38, 19, 84), glm::vec3(0,0,0), glm::vec3(0.0f, 1.0f, 0.0));
+	//view behind jet, needs work
+	//viewingMatrix = glm::lookAt(glm::vec3(pos.x, pos.y + 2, pos.z - 3), pos, glm::vec3(0.0f, 1.0f, 0.0));
 
 	glUniformMatrix4fv(glGetUniformLocation(myShader->handle(), "ViewMatrix"), 1, GL_FALSE, &viewingMatrix[0][0]);
 
@@ -113,13 +116,12 @@ void display()
 	glUniform4fv(glGetUniformLocation(myShader->handle(), "material_specular"), 1, Material_Specular);
 	glUniform1f(glGetUniformLocation(myShader->handle(), "material_shininess"), Material_Shininess);
 
+	glm::mat4 modelmatrix = glm::scale(modelmatrix, glm::vec3(2.0f));
+	terrain.drawElementsUsingVBO(myShader);
+
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-	pos.x += objectRotation[2][0]*0.1;
-	pos.y += objectRotation[2][1]*0.1;
-	pos.z += objectRotation[2][2]*0.1;
-	glm::mat4 modelmatrix = glm::translate(glm::mat4(1.0f), pos);
-	modelmatrix = glm::scale(modelmatrix, glm::vec3(2.0f));
-	ModelViewMatrix = viewingMatrix * modelmatrix * objectRotation;
+	modelmatrix = glm::translate(glm::mat4(1.0f), pos);
+	ModelViewMatrix = viewingMatrix * modelmatrix * objectTransformation;
 	//ModelViewMatrix = viewingMatrix * objectRotation;
 	
 	glUniformMatrix4fv(glGetUniformLocation(myShader->handle(), "ModelViewMatrix"), 1, GL_FALSE, &ModelViewMatrix[0][0]);
@@ -128,8 +130,7 @@ void display()
 	glm::mat3 normalMatrix = glm::inverseTranspose(glm::mat3(ModelViewMatrix));
 	glUniformMatrix3fv(glGetUniformLocation(myShader->handle(), "NormalMatrix"), 1, GL_FALSE, &normalMatrix[0][0]);
 	
-	model.drawElementsUsingVBO(myShader);
-	
+	plane.drawElementsUsingVBO(myShader);	
 	
 	glUseProgram(myBasicShader->handle());  // use the shader
 	glUniformMatrix4fv(glGetUniformLocation(myBasicShader->handle(), "ProjectionMatrix"), 1, GL_FALSE, &ProjectionMatrix[0][0]);
@@ -161,7 +162,7 @@ void reshape(int width, int height)		// Resize the OpenGL window
 	glViewport(0,0,width,height);						// Reset The Current Viewport
 
 	//Set the projection matrix
-	ProjectionMatrix = glm::perspective(glm::radians(60.0f), (GLfloat)screenWidth/(GLfloat)screenHeight, 1.0f, 200.0f);
+	ProjectionMatrix = glm::perspective(glm::radians(90.0f), (GLfloat)screenWidth/(GLfloat)screenHeight, 1.0f, 200.0f);
 }
 void init()
 {
@@ -189,10 +190,10 @@ void init()
 
 	//lets initialise our object's rotation transformation 
 	//to the identity matrix
-	objectRotation = glm::mat4(1.0f);
+	objectTransformation = glm::mat4(1.0f);
 
 	cout << " loading model " << endl;
-	if(objLoader.loadModel("Models/TAL16.obj", model))//returns true if the model is loaded, puts the model in the model parameter
+	if(objLoader.loadModel("Models/TAL16.obj", plane))//returns true if the model is loaded, puts the model in the model parameter
 	{
 		cout << " model loaded " << endl;		
 
@@ -201,14 +202,33 @@ void init()
 		//back so that the centre is on the origin.
 		//model.calcCentrePoint();
 		//model.centreOnZero();
-
 	
-		model.calcVertNormalsUsingOctree();  //the method will construct the octree if it hasn't already been created.
-				
+		plane.calcVertNormalsUsingOctree();  //the method will construct the octree if it hasn't already been created.				
 
 		//turn on VBO by setting useVBO to true in threeDmodel.cpp default constructor - only permitted on 8 series cards and higher
-		model.initDrawElements();
-		model.initVBO(myShader);
+		plane.initDrawElements();
+		plane.initVBO(myShader);
+	}
+	else
+	{
+		cout << " model failed to load " << endl;
+	}
+
+	if (objLoader.loadModel("Models/terrain.obj", terrain))//returns true if the model is loaded, puts the model in the model parameter
+	{
+		cout << " model loaded " << endl;
+
+		//if you want to translate the object to the origin of the screen,
+		//first calculate the centre of the object, then move all the vertices
+		//back so that the centre is on the origin.
+		//model.calcCentrePoint();
+		//model.centreOnZero();
+
+		terrain.calcVertNormalsUsingOctree();  //the method will construct the octree if it hasn't already been created.
+
+		//turn on VBO by setting useVBO to true in threeDmodel.cpp default constructor - only permitted on 8 series cards and higher
+		terrain.initDrawElements();
+		terrain.initVBO(myShader);
 	}
 	else
 	{
@@ -216,8 +236,7 @@ void init()
 	}
 }
 
-void special(int key, int x, int y)
-{
+void special(int key, int x, int y){
 	switch (key)
 	{
 	case GLUT_KEY_LEFT:
@@ -232,17 +251,10 @@ void special(int key, int x, int y)
 	case GLUT_KEY_DOWN:
 		Down = true;
 		break;
-	case GLUT_KEY_HOME:
-		Home = true;
-		break;
-	case GLUT_KEY_END:
-		End = true;
-		break;
 	}
 }
 
-void specialUp(int key, int x, int y)
-{
+void specialUp(int key, int x, int y){
 	switch (key)
 	{
 	case GLUT_KEY_LEFT:
@@ -256,13 +268,38 @@ void specialUp(int key, int x, int y)
 		break;
 	case GLUT_KEY_DOWN:
 		Down = false;
-		break;
-	case GLUT_KEY_HOME:
-		Home = false;
-		break;
-	case GLUT_KEY_END:
-		End = false;
-		break;		
+		break;	
+	}
+}
+
+void keyFunc(unsigned char key, int x, int y){
+	switch (key) {
+		case 97:
+			A = true;
+			break;
+		case 100:
+			D = true;
+			break;
+		case 32:
+			SPACE = true;
+			break;
+		case 27:
+			exit(0);
+			break;
+	}
+}
+
+void keyUpFunc(unsigned char key, int x, int y) {
+	switch (key) {
+		case 97:
+			A = false;
+			break;
+		case 100:
+			D = false;
+			break;
+		case 32:
+			SPACE = false;
+			break;
 	}
 }
 
@@ -271,11 +308,11 @@ void processKeys()
 	float spinXinc = 0.0f, spinYinc = 0.0f, spinZinc = 0.0f;
 	if (Left)
 	{
-		spinYinc = -0.1f;
+		spinYinc = 0.1f;
 	}
 	if (Right)
 	{
-		spinYinc = 0.1f;
+		spinYinc = -0.1f;
 	}
 	if (Up)
 	{
@@ -285,30 +322,32 @@ void processKeys()
 	{
 		spinXinc = -0.1f;
 	}
-	if (Home)
+	if (A)
+	{
+		spinZinc = -0.1f;
+	}
+	if (D)
 	{
 		spinZinc = 0.1f;
 	}
-	if (End)
+	if (SPACE)
 	{
-		spinZinc = -0.1f;
+		pos.x += objectTransformation[2][0] * 0.1;
+		pos.y += objectTransformation[2][1] * 0.1;
+		pos.z += objectTransformation[2][2] * 0.1;
 	}
 	updateTransform(spinXinc, spinYinc, spinZinc);
 }
 
 void updateTransform(float xinc, float yinc, float zinc)
 {
-	objectRotation = glm::rotate(objectRotation, xinc, glm::vec3(1,0,0));
-	objectRotation = glm::rotate(objectRotation, yinc, glm::vec3(0,1,0));
-	objectRotation = glm::rotate(objectRotation, zinc, glm::vec3(0,0,1));
+	objectTransformation = glm::rotate(objectTransformation, xinc, glm::vec3(1,0,0));
+	objectTransformation = glm::rotate(objectTransformation, yinc, glm::vec3(0,1,0));
+	objectTransformation = glm::rotate(objectTransformation, zinc, glm::vec3(0,0,1));
 }
 
 void idle()
 {
-	spin += speed;
-	if(spin > 360)
-		spin = 0;
-
 	processKeys();
 
 	glutPostRedisplay();
@@ -331,13 +370,6 @@ int main(int argc, char **argv)
 		cout << " GLEW ERROR" << endl;
 	}
 
-	//Check the OpenGL version being used
-	int OpenGLVersion[2];
-	glGetIntegerv(GL_MAJOR_VERSION, &OpenGLVersion[0]);
-	glGetIntegerv(GL_MINOR_VERSION, &OpenGLVersion[1]);
-	cout << OpenGLVersion[0] << " " << OpenGLVersion[1] << endl;
-
-
 	//initialise the objects for rendering
 	init();
 
@@ -347,6 +379,9 @@ int main(int argc, char **argv)
 
 	glutSpecialFunc(special);
 	glutSpecialUpFunc(specialUp);
+
+	glutKeyboardFunc(keyFunc);
+	glutKeyboardUpFunc(keyUpFunc);
 
 	glutIdleFunc(idle);
 
